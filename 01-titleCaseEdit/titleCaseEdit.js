9215d9d1687efcfaacc980bb4e1c8d21b4a28995
Junyao Han <101294134+junyaokh822@users.noreply.github.com>
2023-05-24 23:31:44 -0400

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,8 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  const words = title.toLowerCase().split(' ');
+  const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+  const revisedTitle = capitalizedWords.join(' ');
+  return revisedTitle;
 }
 
 // Do not edit this line;
