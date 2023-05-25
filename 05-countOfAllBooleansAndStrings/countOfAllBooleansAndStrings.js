7599bed8225fafb024e7297b5c23e294e8d96966
Junyao Han <101294134+junyaokh822@users.noreply.github.com>
2023-05-25 19:39:19 -0400

    Update countOfAllBooleansAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,12 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (typeof element === 'boolean' || typeof element === 'string') {
+      count++;
+    }
+  }
+  return count;
 }
 
 // Do not edit this line;
