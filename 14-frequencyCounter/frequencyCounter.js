9e698b8a951fa780c0e4d10feba831193f203855
Junyao Han <101294134+junyaokh822@users.noreply.github.com>
2023-05-25 22:02:38 -0400

    Update frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,16 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  var frequencies = {};  
+  for (var i = 0; i < word.length; i++) {
+    var char = word[i];
+    if (frequencies[char]) {
+      frequencies[char]++;
+    } else {
+      frequencies[char] = 1;
+    }
+  }  
+  return frequencies;
 }
+
 
 // Do not edit this line;
 module.exports = frequencyCounter;
