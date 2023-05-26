1ececf0ecd14500a2ec4abc550d69b765bee234e
Junyao Han <101294134+junyaokh822@users.noreply.github.com>
2023-05-25 22:45:20 -0400

    Update binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -1,12 +1,25 @@
 class MySolution {
   constructor() {
-    this.flag = false; // this is optional to use;
+    this.flag = false;
   }
-
   binarySearch(nums, target) {
-    // Insert code here;
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2);    
+      if (nums[mid] === target) {
+        this.flag = true;
+        break;
+      } else if (nums[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return this.flag;
   }
 }
+
 
 // Do not edit this line;
 let StudentSolution = MySolution;
