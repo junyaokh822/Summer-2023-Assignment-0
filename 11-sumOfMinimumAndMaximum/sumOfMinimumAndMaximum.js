74beb355a9f01e1178e9e969aed7213493254fdd
Junyao Han <101294134+junyaokh822@users.noreply.github.com>
2023-05-25 21:45:35 -0400

    Update sumOfMinimumAndMaximum.js

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,6 +1,17 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  let minimum = nums[0];
+  let maximum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < minimum) {
+      minimum = nums[i];
+    }
+    if (nums[i] > maximum) {
+      maximum = nums[i];
+    }
+  }
+  return minimum + maximum;
 }
+
 
 // Do not edit this line;
 module.exports = sumOfMinimumAndMaximum;
